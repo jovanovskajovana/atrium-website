1ac75fe6854b21a60ad1969c303c79352ecb216b
Jovana Jovanovska <jovanovska.jovana1@gmail.com>
2026-02-19 17:36:58 -0600

    Update logo svgs

--- src/components/icons/atrium-logo-step-2.tsx
+++ src/components/icons/atrium-logo-step-2.tsx
@@ -5,17 +5,17 @@ import { IconProps } from '@/interfaces/ui'
 const AtriumLogoStep2: FC<IconProps> = ({ className }) => (
   <svg
     width="1256"
-    height="613"
-    viewBox="0 0 1256 613"
+    height="660"
+    viewBox="0 0 1256 660"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     preserveAspectRatio="none"
     className={className}
   >
-    <path d="M1256 0H632V64H882.411H944H1005.59H1256V0Z" fill="#141715" />
+    <path d="M1256 68.8135H749.788V0H1256V68.8135Z" fill="#1d1d1b" />
     <path
-      d="M346.962 0H277.944L250.411 64.24L0 613H70.9782L114.216 517.456L171.191 390.534H170.897L216.135 290.435H216.162L312 64.24L402.076 282.817L402.156 282.95L450.489 390.534H450.169L506.85 517.536L549.501 613H624L373.589 64.24L346.962 0Z"
-      fill="#141715"
+      d="M357.734 0H290.387L0 660H73.7076L323.146 86.1064L570.755 660H648.121L357.734 0Z"
+      fill="#1d1d1b"
     />
   </svg>
 )
--- src/components/icons/atrium-logo-step-3.tsx
+++ src/components/icons/atrium-logo-step-3.tsx
@@ -5,16 +5,16 @@ import { IconProps } from '@/interfaces/ui'
 const AtriumLogoStep3: FC<IconProps> = ({ className }) => (
   <svg
     width="1256"
-    height="613"
-    viewBox="0 0 1256 613"
+    height="660"
+    viewBox="0 0 1256 660"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     preserveAspectRatio="none"
     className={className}
   >
     <path
-      d="M940 0H316V63.9119H566.411L316 613H386.978L430.216 517.399L487.191 390.401H486.897L532.135 290.242H532.162L628 63.9119L718.076 282.62L718.156 282.753L766.489 390.401H766.169L822.85 517.479L865.501 613H940L689.589 63.9119H940V0Z"
-      fill="#141715"
+      d="M963 0H662.826H593.174H293V68.8122H561.871L293 660H369.211L415.636 557.069L476.81 420.334H476.495L525.068 312.496H525.097L628 68.8122L724.717 304.289L724.803 304.432L776.698 420.334H776.354L837.214 557.155L883.009 660H963L694.129 68.8122H963V0Z"
+      fill="#1d1d1b"
     />
   </svg>
 )
