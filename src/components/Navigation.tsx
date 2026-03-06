0910d465b2e2a2012a5de7a29774f7d619a26219
Jovana Jovanovska <jovanovska.jovana1@gmail.com>
2026-03-06 19:18:00 +0100

    Handle active page nav item

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,19 @@
+'use client'
+
 import { useTranslations } from 'next-intl'
 
 import LanguagePicker from '@/components/LanguagePicker'
 import AtriumLogo from '@/components/icons/atrium-logo'
 
-import { Link } from '@/i18n/navigation'
+import { NAV_ITEMS } from '@/constants/navigation'
+
+import { Link, usePathname } from '@/i18n/navigation'
 
 const Navigation = () => {
   const t = useTranslations('nav')
+  const pathname = usePathname()
+
+  const isHome = pathname === '/'
 
   return (
     <>
@@ -19,48 +26,17 @@ const Navigation = () => {
       </Link>
 
       <nav className="absolute top-[16vh] left-[3vw]" data-nav>
-        <Link
-          href="/references"
-          className="flex text-black-100 text-[1vw] font-[450] leading-[1.8]"
-          data-menu-item
-        >
-          {t('references')}
-        </Link>
-        <Link
-          href="/production"
-          className="flex text-black-100 text-[1vw] font-[450] leading-[1.8]"
-          data-menu-item
-        >
-          {t('production')}
-        </Link>
-        <Link
-          href="/news"
-          className="flex text-black-100 text-[1vw] font-[450] leading-[1.8]"
-          data-menu-item
-        >
-          {t('news')}
-        </Link>
-        <Link
-          href="/careers"
-          className="flex text-black-100 text-[1vw] font-[450] leading-[1.8]"
-          data-menu-item
-        >
-          {t('careers')}
-        </Link>
-        <Link
-          href="/about"
-          className="flex text-black-100 text-[1vw] font-[450] leading-[1.8]"
-          data-menu-item
-        >
-          {t('about')}
-        </Link>
-        <Link
-          href="/contact"
-          className="flex text-black-100 text-[1vw] font-[450] leading-[1.8]"
-          data-menu-item
-        >
-          {t('contact')}
-        </Link>
+        {NAV_ITEMS.map(({ href, key }) => (
+          <Link
+            key={key}
+            href={href}
+            className="flex text-black-100 text-[1vw] font-[450] leading-[1.8]"
+            data-menu-item
+            data-active={!isHome && pathname.startsWith(href) ? '' : undefined}
+          >
+            {t(key)}
+          </Link>
+        ))}
       </nav>
 
       <div className="absolute top-[4vh] right-[3vw]" data-lang-picker>
---
+++ src/constants/navigation.ts
@@ -0,0 +1,8 @@
+export const NAV_ITEMS = [
+  { href: '/references', key: 'references' },
+  { href: '/production', key: 'production' },
+  { href: '/news', key: 'news' },
+  { href: '/careers', key: 'careers' },
+  { href: '/about', key: 'about' },
+  { href: '/contact', key: 'contact' },
+] as const
