471c608d8205a420fcb37ff24688079dd62416ed
Jovana Jovanovska <jovanovska.jovana1@gmail.com>
2026-03-31 21:13:48 +0200

    Revert "Fix lint error"
    This reverts commit ceb6878cbd1162e157af1d7db8f4e07fe3da1918.

--- src/app/[locale]/references/[slug]/page.tsx
+++ src/app/[locale]/references/[slug]/page.tsx
@@ -1,19 +1,18 @@
 import { notFound } from 'next/navigation'
-import { getTranslations } from 'next-intl/server'
+import { useTranslations } from 'next-intl'
 
 import { PROJECTS } from '@/constants/projects'
 
 interface Props {
-  params: Promise<{ slug: string }>
+  params: { slug: string }
 }
 
-const ProjectPage = async ({ params }: Props) => {
-  const { slug } = await params
-  const project = PROJECTS.find((p) => p.slug === slug)
+const ProjectPage = ({ params }: Props) => {
+  const project = PROJECTS.find((p) => p.slug === params.slug)
 
   if (!project) return notFound()
 
-  const t = await getTranslations('home')
+  const t = useTranslations('home')
   const index = PROJECTS.indexOf(project)
 
   return (
